deb600c8f03b25354c4e55dd1603d2d8fb319332
nisaking03 <nisa.king03@gmail.com>
2025-12-15 05:56:04 -0500

    making sure i pushed

--- src/main/java/org/yearup/controllers/ProductsController.java
+++ src/main/java/org/yearup/controllers/ProductsController.java
@@ -81,7 +81,8 @@ public class ProductsController
     {
         try
         {
-            productDao.create(product);
+            productDao.update(id,product);
+            // changed .create to .update
         }
         catch(Exception ex)
         {
